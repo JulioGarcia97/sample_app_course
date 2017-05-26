95d4b7c3ab6167c8c5a20a99ff821ee7d7b2432b
Julio Garcia <cggjulio@gmail.com>
2017-05-26 17:24:10 -0500

    Refactor generic title to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	def full_title(page_title = '')
+		default_title = 'Sample App'
+
+		if page_title.empty?
+			default_title
+		else
+			page_title + ' | ' + default_title
+		end
+
+	end
 end
