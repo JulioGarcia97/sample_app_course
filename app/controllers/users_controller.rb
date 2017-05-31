dafe4604619efed45c9d523fb641b76b747163b2
Julio Garcia <cggjulio@gmail.com>
2017-05-30 19:23:19 -0500

    edit & update funcionality for users account | Basic validation for users authenticate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
-
+	before_action :set_user, only: [:show, :edit, :update, :delete]
+	before_action :logged_in_user, only: [:edit, :update]
+	before_action :correct_user, only: [:edit, :update]
 	def new
 		@user = User.new
 		@page_title = 'Sign Up'
 	end
 
 	def show
-		@user = User.find(params[:id])
 		@page_title = @user.name
 	end
 
@@ -22,9 +23,40 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def edit
+		@page_title = 'Edit Account'
+	end
+
+	def update
+		@page_title = 'Edit Account'
+		if @user.update(user_params)
+			flash[:success] = 'Se actualizo correctamente tu perfil'
+			redirect_to @user
+		else
+			render :edit
+		end
+	end
+
 	private
+
+	def set_user
+		@user = User.find(params[:id])
+	end
 
 	def user_params
 		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
+
+	def logged_in_user
+		unless logged_in?
+			store_location
+			flash[:danger] = 'Necesitas tener cuenta para acceder a esa pagina'
+			redirect_to login_path
+		end
+	end 
+
+	def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 end
